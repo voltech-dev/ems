1b700ce451b3b56e8808920add54245f6fe96f78
Nehru <nehru.k@VG.COM>
2020-11-08 12:56:55 +0530

    emp_view

--- resources/views/EmpDetails/emp_detailsview.blade.php
+++ resources/views/EmpDetails/emp_detailsview.blade.php
@@ -27,24 +27,11 @@ error_reporting(0);
 
 @section('content')
 <div class="row">
-    <div class="col-xl-12 col-lg-12 col-md-12">
+    <div class="col-md-10">
         <div class="card">
             <div class="card-header" style="color: #31708f;background-color: #d9edf7; border-color: #bce8f1;">
                 <h3 class="card-title">Employee Id: {{$model-> emp_code}}</h3>
-                <!-- <div class="card-options">
-                    <a href="https://laravel.spruko.com/admitro/Horizontal-Light/#" class="option-dots"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
-                            class="fe fe-more-horizontal fs-20"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="https://laravel.spruko.com/admitro/Horizontal-Light/#">Today</a>
-                        <a class="dropdown-item" href="https://laravel.spruko.com/admitro/Horizontal-Light/#">Last
-                            Week</a>
-                        <a class="dropdown-item" href="https://laravel.spruko.com/admitro/Horizontal-Light/#">Last
-                            Month</a>
-                        <a class="dropdown-item" href="https://laravel.spruko.com/admitro/Horizontal-Light/#">Last
-                            Year</a>
-                    </div>
-                </div>-->
+
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -89,34 +76,161 @@ error_reporting(0);
 
                             </tr>
                             <tr>
-                            <td class="font-weight-bold">Last Appraisal Date</td>
+                                <td class="font-weight-bold">Last Appraisal Date</td>
                                 <td><span class="badge badge-info">{{$model->last_appraisal_date}}</span></td>
-                                <td class="font-weight-bold">Reason for Leaving	</td>
+                                <td class="font-weight-bold">Reason for Leaving </td>
                                 <td><span class="badge badge-info">{{$model->reason_for_leaving}}</span></td>
-                               
+
                             </tr>
                             <tr>
-                            <td class="font-weight-bold">Reporting Authority </td>
+                                <td class="font-weight-bold">Reporting Authority </td>
                                 <td><span class="badge badge-info">{{$model->reporting_authority_id}}</span></td>
                                 <td class="font-weight-bold">Status</td>
                                 <td><span class="badge badge-info">{{$model->status_id}}</span></td>
                             </tr>
-                           
+
                         </tbody>
+                        <?php 
+                        $remunerat = App\Models\EmpRemunerationDetails::where(['empid'=>$model->id])->first();
+                        ?>
                         <thead class="">
                             <tr>
-                                <th colspan=4>General</th>
+                                <th colspan=4>Remuneration</th>
+
 
                             </tr>
                         </thead>
                         <tbody>
+                            <tr>
+                                <td class="font-weight-bold">Salary Structure</td>
+                                <td><span class="badge badge-info">{{$remunerat->salary_structure}}</span></td>
+                                <td class="font-weight-bold">Esi Applicability</td>
+                                <td><span class="badge badge-info">{{$remunerat->esi_applicability}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Pf Applicablity</td>
+                                <td><span class="badge badge-info">{{$remunerat->pf_applicablity}}</span></td>
+                                <td class="font-weight-bold">Restrict Pf</td>
+                                <td><span class="badge badge-info">{{$remunerat->restrict_pf}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Basic</td>
+                                <td><span class="badge badge-info">{{$remunerat->basic}}</span></td>
+                                <td class="font-weight-bold">HRA</td>
+                                <td><span class="badge badge-info">{{$remunerat->hra}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Splallowance</td>
+                                <td><span class="badge badge-info">{{$remunerat->splallowance}}</span></td>
+                                <td class="font-weight-bold">Dearness Allowance</td>
+                                <td><span class="badge badge-info">{{$remunerat->Dearness_allowance}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Conveyance</td>
+                                <td><span class="badge badge-info">{{$remunerat->conveyance}}</span></td>
+                                <td class="font-weight-bold">Lta</td>
+                                <td><span class="badge badge-info">{{$remunerat->lta}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Medical</td>
+                                <td><span class="badge badge-info">{{$remunerat->medical}}</span></td>
+                                <td class="font-weight-bold">Other Allowance</td>
+                                <td><span class="badge badge-info">{{$remunerat->other_allowance}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Gross Salary</td>
+                                <td><span class="badge badge-info">{{$remunerat->gross_salary}}</span></td>
+                                <td class="font-weight-bold"></td>
+                                <td><span class="badge badge-info"></span></td>
+                            </tr>
+                        </tbody>
+                        <?php 
+                        $statue = App\Models\EmpStatutorydetails::where(['empid'=>$model->id])->first();
+                        ?>
+                        <thead class="">
+                            <tr>
+                                <th colspan=4>Statutory</th>
+
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="font-weight-bold">ESI No</td>
+                                <td><span class="badge badge-info">{{$statue->esino}}</span></td>
+                                <td class="font-weight-bold">ESI Dispensary</td>
+                                <td><span class="badge badge-info">{{$statue->esidispensary}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">EPF No</td>
+                                <td><span class="badge badge-info">{{$statue->epfno}}</span></td>
+                                <td class="font-weight-bold">EPF UAN No</td>
+                                <td><span class="badge badge-info">{{$statue->epfuanno}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Professional Tax</td>
+                                <td><span class="badge badge-info">{{$statue->professionaltax}}</span></td>
+                                <td class="font-weight-bold"></td>
+                                <td><span class="badge badge-info"></span></td>
+                            </tr>
+                            <tr>
+
+                        </tbody>
+                        <?php 
+                        $bank = App\Models\EmpBankdetails::where(['empid'=>$model->id])->first();
+                        ?>
+                        <thead class="">
+                            <tr>
+                                <th colspan=4>Bank</th>
+
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="font-weight-bold">Bank Name</td>
+                                <td><span class="badge badge-info">{{$bank->bankname}}</span></td>
+                                <td class="font-weight-bold">AC Number</td>
+                                <td><span class="badge badge-info">{{$bank->acnumber}}</span></td>
+                            </tr>
+                            <tr>
+                                <td class="font-weight-bold">Branch</td>
+                                <td><span class="badge badge-info">{{$bank->branch}}</span></td>
+                                <td class="font-weight-bold">IFSC</td>
+                                <td><span class="badge badge-info">{{$bank->ifsc}}</span></td>
+                            </tr>
+
+
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header" style="color: #fff;background-color: #337ab7; border-color: #337ab7;">
+                <h3 class="card-title">Documents</h3>
+
+            </div>
+           
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Appointment Generate</a>
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Appointment Pdf</a>
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Annexure</a>
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Bonafide </a>
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Relieving Letter</a>
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Show Cause Notice</a>
+                <a  class="list-group-item" href="{{ url('/applicants') }}">Employee Tracking</a>
+
+           
+        </div>
+    </div>
+
 </div>
+
+
+
+
 
 
 
