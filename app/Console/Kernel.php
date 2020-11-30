cea10423f4f5ab56d81c21689c6942146fd85f35
JP <prakashv85@gmail.com>
2020-11-30 14:56:27 +0530

    scheduling runbackground

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->call('App\Http\Controllers\EmployeeController@mail')->everyMinute();
+         $schedule->call('App\Http\Controllers\EmployeeController@mail')->everyMinute()->runInBackground();
     }
 
     /**
