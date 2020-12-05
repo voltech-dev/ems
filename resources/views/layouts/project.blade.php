14b5cb3c3cdc6d42d5b7ed42b5bf531e2badda53
JP <prakashv85@gmail.com>
2020-12-05 14:13:00 +0530

    project template updated

--- resources/views/layouts/project.blade.php
+++ resources/views/layouts/project.blade.php
@@ -155,7 +155,7 @@
                                         <span class="text-center user-semi-title"></span>
                                         <div class="dropdown-divider"></div>
                                     </div>
-                                    <a class="dropdown-item d-flex" href="{{ url('site/profile') }}">
+                                    <!--  <a class="dropdown-item d-flex" href="{{ url('site/profile') }}">
                                         <svg class="header-icon mr-3" xmlns="http://www.w3.org/2000/svg" height="24"
                                             viewBox="0 0 24 24" width="24">
                                             <path d="M0 0h24v24H0V0z" fill="none" />
@@ -164,7 +164,7 @@
                                         </svg>
                                         <div class="">Profile</div>
                                     </a>
-                                    <!--<a class="dropdown-item d-flex" href="#">
+                                  <a class="dropdown-item d-flex" href="#">
 											<svg class="header-icon mr-3" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
 											<div class="">Settings</div>
 										</a>
@@ -222,7 +222,7 @@
                                     </a>
                                 </li>
                                 <li aria-haspopup="true">
-                                    <a href="{{ url('/EmpDetails') }}" class="">
+                                    <a href="{{ url('/employee-index') }}" class="">
                                         <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" height="24"
                                             viewBox="0 0 24 24" width="24">
                                             <path d="M0 0h24v24H0V0z" fill="none" />
@@ -233,7 +233,7 @@
                                     </a>
                                 </li>
                                 <li aria-haspopup="true">
-                                    <a href="{{ url('/superuser_attendance') }}" class="sub-icon">
+                                    <a href="{{ url('/attendance-show') }}" class="sub-icon">
                                         <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" height="24"
                                             viewBox="0 0 24 24" width="24">
                                             <path d="M0 0h24v24H0V0z" fill="none" />
@@ -244,7 +244,7 @@
                                     </a>
                                 </li>    
                                  <li aria-haspopup="true">
-                                    <a href="{{ url('/superuser_leavemgmt') }}" class="sub-icon">
+                                    <a href="{{ url('/leave-show') }}" class="sub-icon">
                                         <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" height="24"
                                             viewBox="0 0 24 24" width="24">
                                             <path d="M0 0h24v24H0V0z" fill="none" />
@@ -252,43 +252,6 @@
                                                 d="M19 15v4H5v-4h14m1-2H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1zM7 18.5c-.82 0-1.5-.67-1.5-1.5s.68-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM19 5v4H5V5h14m1-2H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zM7 8.5c-.82 0-1.5-.67-1.5-1.5S6.18 5.5 7 5.5s1.5.68 1.5 1.5S7.83 8.5 7 8.5z" />
                                         </svg>
                                         Leave Management
-                                    </a>
-                                </li>
-                                <li aria-haspopup="true">
-                                    <a href="#" class="sub-icon">
-                                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" height="24"
-                                            viewBox="0 0 24 24" width="24">
-                                            <path d="M0 0h24v24H0V0z" fill="none" />
-                                            <path
-                                                d="M4 5h3l2-2h6l2 2h3a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2zm12 12v-1c0-1.332-2.667-2-4-2-1.333 0-4 .668-4 2v1h8zm-4-8a2.001 2.001 0 0 0 0 4 2.001 2.001 0 0 0 0-4z" />
-                                        </svg>
-                                        Recruitment
-                                    </a>
-                                </li>
-                                <li aria-haspopup="true">
-                                    <a href="#" class="sub-icon">
-                                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" height="24"
-                                            viewBox="0 0 24 24" width="24">
-                                            <path d="M0 0h24v24H0V0z" fill="none" />
-                                            <path
-                                                d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z" />
-                                        </svg>
-                                        HR
-                                    </a>
-                                </li>
-                                <li aria-haspopup="true">
-                                    <a href="#" class="sub-icon">
-                                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" height="24"
-                                            viewBox="0 0 24 24" width="24">
-                                            <path d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M12 22C6.49 22 2 17.51 2 12S6.49 2 12 2s10 4.04 10 9c0 3.31-2.69 6-6 6h-1.77c-.28 0-.5.22-.5.5 0 .12.05.23.13.33.41.47.64 1.06.64 1.67 0 1.38-1.12 2.5-2.5 2.5zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8c.28 0 .5-.22.5-.5 0-.16-.08-.28-.14-.35-.41-.46-.63-1.05-.63-1.65 0-1.38 1.12-2.5 2.5-2.5H16c2.21 0 4-1.79 4-4 0-3.86-3.59-7-8-7z" />
-                                            <circle cx="6.5" cy="11.5" r="1.5" />
-                                            <circle cx="9.5" cy="7.5" r="1.5" />
-                                            <circle cx="14.5" cy="7.5" r="1.5" />
-                                            <circle cx="17.5" cy="11.5" r="1.5" />
-                                        </svg>
-                                        Settings
                                     </a>
                                 </li>
                             </ul>
